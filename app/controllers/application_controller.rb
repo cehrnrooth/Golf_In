dc5471e5d2957a720f662f0fb2fd33c0ed5ceb78
Carl <carl.ehrnrooth@gmail.com>
2012-10-21 21:51:44 -0700

    fixing 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
   before_filter :keys
   before_filter :sign_in?, :except => [:home, :images, :about]
   
-  unless Rails.application.config.consider_all_requests_local
-    #rescue_from Exception, with: :render_500
-    rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from ActionController::UnknownAction, with: :render_404
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  end
+  #unless Rails.application.config.consider_all_requests_local
+  #  rescue_from Exception, with: :render_500
+  #  rescue_from ActionController::RoutingError, with: :render_404
+  #  rescue_from ActionController::UnknownController, with: :render_404
+  #  rescue_from ActionController::UnknownAction, with: :render_404
+  #  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  #end
   
   def keys
     keys = {:api_key => "f8uxc1elzyek", :secret_key => "heWxLmV4NVa6ybaI"}
@@ -33,12 +33,12 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    #def render_500(exception)
-    #  @error = exception
-    #  respond_to do |format|
-    #    format.html { render template: 'errors/error_500', layout: 'layouts/application', status: 500 }
-    #    format.all { render nothing: true, status: 500}
-    #  end
-    #end
+    def render_500(exception)
+      @error = exception
+      respond_to do |format|
+        format.html { render template: 'errors/error_500', layout: 'layouts/application', status: 500 }
+        format.all { render nothing: true, status: 500}
+      end
+    end
   
 end
